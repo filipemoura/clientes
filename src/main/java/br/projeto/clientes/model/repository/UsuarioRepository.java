f56100a2998de15f4fda8cd5eb2fea37f0259883
Filipe Alves <filipe.alves@sefin.fortaleza.ce.gov.br>
2021-03-31 07:07:59 -0300

    Atualização da segurança e login de usuário

--- src/main/java/br/projeto/clientes/model/repository/UsuarioRepository.java
+++ src/main/java/br/projeto/clientes/model/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.projeto.clientes.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.projeto.clientes.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+	
+	Optional<Usuario> findByUsername(String username);
 
 }
