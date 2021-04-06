7a07042666819c334bb325cb3bb8f46abbc4d799
Filipe Alves <filipe.alves@sefin.fortaleza.ce.gov.br>
2021-04-06 07:11:43 -0300

    Melhoria nas mensagens de cadastro do usuário

--- src/main/java/br/projeto/clientes/UsuarioCadastradoException.java
+++ src/main/java/br/projeto/clientes/UsuarioCadastradoException.java
@@ -8,7 +8,7 @@ public class UsuarioCadastradoException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
 	public UsuarioCadastradoException(String login) {
-		super("Usuário já cadastrado para o login: " + login);
+		super("Usuário já cadastrado para o login " + login);
 	}
 
 }
